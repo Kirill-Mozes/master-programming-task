5aa842b6762ac02ff49b979967106a921789b838
Kirill-Mozes <mozin.kirill@yandex.ru>
2020-09-25 23:03:34 +0300

    lab is done

--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -9,16 +9,65 @@
 
 #include <iostream>
 
-//{ endm manipulator
+using namespace std;
 
+//{ endm manipulator
+inline ostream& endm(ostream& str) {
+	str << "[eol]\n";
+	return str;
+}
 //}
 
 //{ squares manipulator
+class SquaresProxy {
+public:
+	explicit SquaresProxy(ostream & os) :os(os) {}
+	ostream & os;
+};
 
+template<typename Arg>
+	 ostream & operator<<(SquaresProxy const& a, Arg const& arg) {
+		a.os << "[" << arg << "]";
+		return a.os;
+	}
+
+inline class SquaresCreator { } squares;
+
+inline SquaresProxy operator<<(ostream & os, SquaresCreator) {
+	return SquaresProxy(os);
+}
 //}
 
 //{ add manipulator
+class Adder {
+public:
+	struct farg {
+	ostream& ostr;
+	};
 
+	template<typename T>
+	struct sarg {
+		ostream& ostr;
+		T value;
+	};
+};
+
+inline Adder::farg operator<< (ostream& ostr, Adder) {
+	return {ostr};
+}
+
+template<typename T>
+Adder::sarg<T> operator<< (Adder::farg fag, T&& fval) {
+	return {fag.ostr, fval};
+}
+
+template<typename TL, typename TR>
+ostream& operator<< (Adder::sarg<TL> sag, TR&& sval) {
+	sag.ostr << (sag.value + sval);
+	return sag.ostr;
+}
+
+inline Adder add;
 //}
 
 #endif // __IOMANIP_HPP__
