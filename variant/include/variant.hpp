574517b2d65ac6017d81022464d7521c64ee9578
Kirill-Mozes <mozin.kirill@yandex.ru>
2020-11-19 00:46:34 +0300

    lab variant

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -15,49 +15,74 @@
 #include <boost/variant.hpp>
 
 //{ number
-struct number
+struct number : public std::variant<int,float>
 {
-
+    using std::variant<int,float>::variant;
 };
 //}
 
 //{ array
-struct array
+struct array : public std::vector<number>
 {
-
+    using std::vector<number>::vector;
 };
 //}
 
 //{ recursive_array
-struct recursive_array
+struct recursive_array : public std::vector<std::variant<number,std::shared_ptr<recursive_array>>>
 {
-
+    using std::vector<std::variant<number,std::shared_ptr<recursive_array>>>::vector;
 };
 //}
 
 //{ recursive_array2
-struct recursive_array2
+struct recursive_array2 : public std::vector<std::variant<number,boost::recursive_wrapper<recursive_array2>>>
 {
-
+    using std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>::vector;
 };
 //}
 
 //{ variant_decorator
-template<???>
-struct variant_decorator
+template<typename... Types>
+struct variant_decorator : public std::variant<Types...>
 {
+    using std::variant<Types...>::variant;
 
-    ?? as()
+    template<typename ReturnType> 
+    ReturnType& as()
     {
-
+        
+        if constexpr (std::disjunction_v<std::is_same<boost::recursive_wrapper<ReturnType>, Types>...>)
+        {
+            return std::get<boost::recursive_wrapper<ReturnType>>(*this).get();
+        }
+        else
+        {
+            return std::get<ReturnType>(*this);
+        }
     }
+
+    
+    template<typename ReturnType>
+    ReturnType as() const
+    {
+        if constexpr (std::disjunction_v<std::is_same<boost::recursive_wrapper<ReturnType>, Types>...>)
+        {
+            return std::get<boost::recursive_wrapper<ReturnType>>(*this).get();
+        }
+        else
+        {
+            return std::get<ReturnType>(*this);
+        }
+    }
+
 };
 //}
 
 //{ recursive_map
-struct recursive_map
+struct recursive_map : public std::map<std::string, variant_decorator<int, std::string, bool, boost::recursive_wrapper<recursive_map>>>
 {
-
+    using std::map<std::string, variant_decorator<int, std::string, bool, boost::recursive_wrapper<recursive_map>>>::map;
 };
 //}
 
