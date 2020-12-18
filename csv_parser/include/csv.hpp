d6186a4054fe80ff49b1850568e662da28302e6e
Kirill-Mozes <mozin.kirill@yandex.ru>
2020-12-18 17:28:49 +0300

    Lab 5 CSV

--- csv_parser/include/csv.hpp
+++ csv_parser/include/csv.hpp
@@ -37,7 +37,8 @@ namespace types
     {
         //? Which kind of types should I use to describe the CSV type?
         //{
-        ... csv = ...
+        using row = std::vector<std::string>;
+        using csv = std::vector<row>;
         //}
     }
 }
@@ -51,15 +52,14 @@ namespace parser
         namespace x3 = boost::spirit::x3;
 
         //{ csv grammar
-        ... string = ...
-        ... cell = ...
-                 = ...
+        auto const string = x3::rule<class string, std::string>{} = x3::lexeme[+(x3::char_ - (x3::lit(',') | '"' | '\n') )];
+        auto const cell = x3::rule<class cell, std::string>{} = quoted_string | string;
 
-        ... row = ...
-                = ...
+        auto const row = x3::rule<class row, std::vector<std::string>>{} = cell >> *(',' >> cell) >> -x3::no_skip["\n"];
+        auto const header = x3::rule<class row, std::vector<std::string>>{} = row;
+        auto const csv = x3::rule<class csv, types::csv::csv>{} = header >> *row;
 
-        ... csv = ...
-                = ...
+        //BOOST_SPIRIT_DEFINE(csv);
         //}
     }
 }
@@ -69,11 +69,13 @@ namespace literals
     namespace csv
     {
         //{ declare ``_csv`` literal
-        ... _csv ..
-        {
 
+        types::csv::csv operator""_csv(const char* data, size_t)
+        {
+            return parser::load_from_string<types::csv::csv>(data, parser::csv::csv);
         }
         //}
+        
     }
 }
 
--- parser_helper/include/variant_decorator.hpp
+++ parser_helper/include/variant_decorator.hpp
@@ -16,18 +16,37 @@ namespace x3 = boost::spirit::x3;
 //? How many constructors did I inherit?
 //? Do I need my own constructor & ``operator=``? Why?
 //{
-template<???>
-struct variant_decorator: public x3::variant<???>
+template<typename... Types>
+struct variant_decorator : public x3::variant<Types...>
 {
-    using ???operator=;
-    using ...
-
+    using x3::variant<Types...>::operator=;
+    using x3::variant<Types...>::variant;
     //? Do I need a ``const``-version?
     template<class T>
     T& as()
     {
-        return ...
+        if constexpr (std::disjunction_v<std::is_same<boost::recursive_wrapper<T>, Types>...>)
+        {
+            return boost::get<boost::recursive_wrapper<T>>(*this).get();
+        }
+        else
+        {
+            return boost::get<T>(*this);
+        }
     }
-//}
+
+    template<class T>
+    T as() const
+    {
+        if constexpr ((std::is_same_v<boost::recursive_wrapper<T>, Types> || ...))
+        {
+            return boost::get<boost::recursive_wrapper<T>>(*this).get();
+        }
+        else
+        {
+            return boost::get<T>(*this);
+        }
+    }
+};
 
 #endif // __VARIANT_DECORATOR_HPP__
